905beab7be443ef1068939a075f19a133a26adc6
dabtt6 <datp617@gmail.com>
2026-02-22 17:25:01 +0700

    auto update 2026-02-22 17:25:01

--- loop.ps1
+++ loop.ps1
@@ -5,5 +5,5 @@ Write-Host "Javinizer Recurse Loop Started"
 while ($true) {
     Write-Host "Running Recurse..."
     Javinizer -Path '/data' -DestinationPath '/data' -Recurse
-    Start-Sleep -Seconds 300
+    Start-Sleep -Seconds 3000
 }
